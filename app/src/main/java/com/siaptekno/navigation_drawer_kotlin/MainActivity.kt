d8940ed003a6ced9dcdc6bcf6b6b3df4ea0a7cd8
e-haikal <taufanasman@yahoo.com>
2024-07-24 01:18:46 +0700

    first commit

---
+++ app/src/main/java/com/siaptekno/navigation_drawer_kotlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.siaptekno.navigation_drawer_kotlin
+
+import android.os.Bundle
+import android.widget.Toolbar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+// MainActivity.kt
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var navigationView: NavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        }
+    }
+}
+gi
